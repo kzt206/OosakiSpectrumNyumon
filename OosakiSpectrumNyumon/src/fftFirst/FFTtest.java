6398c7b2514092deff0c92234aca88b68af6d134
kzt206 <kzt206@gmail.com>
2020-06-03 20:19:56 +0900

    modify main method

--- OosakiSpectrumNyumon/src/fftFirst/FFTtest.java
+++ OosakiSpectrumNyumon/src/fftFirst/FFTtest.java
@@ -2,11 +2,10 @@ package fftFirst;
 
 public class FFTtest {
 	public static void main(String... args) {
-		FFT fft = new FFT();
 		
 		double[] testData = { 5., 32., 38., -33., -19., -10., 1., -8., -20., 10., -1., 4., 11., -1., -7., -2 };
 
-		
+		FFT fft = new FFT(16, testData, 2.);
 		
 //		fft.cfft(16,testData,2.,16,-1);
 
