391343f8a2af648a57acc4f1f944e5fe10826220
kzt20 <kzt20@DESKTOP-Ryzen3600>
2020-08-02 21:08:41 +0900

    add FIR test program #1

--- OosakiSpectrumNyumon/src/fftFirst/FFTtestEasy.java
+++ OosakiSpectrumNyumon/src/fftFirst/FFTtestEasy.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 
 
+
 public class FFTtestEasy {
 	public static void main(String... args) {
 
@@ -17,9 +18,65 @@ public class FFTtestEasy {
 		fft.ffft(16, testData, 2., 16, -1);
 
 		System.out.println();
-		System.out.println(fft.sinc(3.1415));
+//		System.out.println(fft.sinc(3.1415));
 
 		System.out.println();
+		
+		//20200802
+		double dt = 0.5;
+	    int n = 16;
+		double lower = 0.4;
+		double upper = 0.7;
+		double alpha = 0.1;
+		
+		//元波形のフーリエスペクトル
+		double temp = Math.log(n)/Math.log(2);
+		int nn = (int) Math.pow(2.,Math.ceil(temp));
+		int nfold = nn/2+1;
+		double[] xr0 = new double[nn];
+		double[] xi0 = new double[nn];
+		//データの読み込み
+		for(int i=0;i<n;i++) {
+			xr0[i] = testData[i];
+			xi0[i] = 0.;
+		}
+		for(int i=n;i<nn;i++) {
+			xr0[i] = 0.;
+			xi0[i] = 0.;
+		}
+		//データに対するFFTの計算
+		int ind = -1;
+		FFT fft2 = new FFT(nn, xr0, 2); // 2 = 1/dt
+		double[][] coef = fft.ffft(nn, xr0, 2, nn, ind);
+		System.out.println();
+		for(int i=0;i<nfold;i++) {
+			System.out.println(coef[i][0] + " " + coef[i][1]);
+		}
+		
+		//フィルター
+		double fe1 = lower*dt;
+		double fe2 = upper*dt;
+		double delta = alpha *dt;
+		int j = (int)(3.1/delta+0.5) - 1;
+		if(j%2 == 1) {
+			j = j+1;
+		}
+		temp = Math.log(n+j)/Math.log(2);
+		nn = (int) Math.pow(2.,Math.ceil(temp));
+		nfold = nn/2 + 1;
+		
+		double[] xr = new double[nn];
+		double[] xi = new double[nn];
+		double[] yr = new double[nn];
+		double[] yi = new double[nn];
+		double[] br = new double[nn];
+		double[] bi = new double[nn];
+		double[] b = new double[j+1];
+		double[] w = new double[j+1];
+		//データの再読み込み
+		
+		
+		// end 20200802
 		
 		
 //		//20200725
