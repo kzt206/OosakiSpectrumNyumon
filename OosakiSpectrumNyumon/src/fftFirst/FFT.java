2dd505aad894f6685a02d893422c9b6281a3c0de
kzt206 <kzt206@gmail.com>
2020-08-25 18:26:03 +0900

    Add new cfft method to zero additon and ComplexNumber data

--- OosakiSpectrumNyumon/src/fftFirst/FFT.java
+++ OosakiSpectrumNyumon/src/fftFirst/FFT.java
@@ -109,11 +109,21 @@ public class FFT {
 	 * 
 	 */
 	public Complex[] cfft(int N, double[] data, double samplingF, int ND, int IND) {
+		
+		// get power of 2 (NT) greater than N
+		int NT = 2;
+		while(NT < N) {
+			NT *= 2;
+		}
+		
+		
 		Complex[] complex = new Complex[data.length];
 		for (int i = 0; i < data.length; i++) {
 			complex[i] = new Complex(data[i], 0.);
 		}
 
+		
+		
 		int i = 1;
 		int j = 1;
 		int m = N / 2;
@@ -164,12 +174,100 @@ public class FFT {
 		}
 
 		for (int ii = 0; ii < complex.length; ii++) {
-			complex[ii] = complex[ii].divide(N);
+			complex[ii] = complex[ii].divide(N);  
 		}
 
 		return complex;
 	}
 
+	
+	/**
+	 * Complex Fourier Fast Transform
+	 * 
+	 * @param N         number of data
+	 * @param data      Input data
+	 * @param samplingF Sampling frequency
+	 * @param ND        not used
+	 * @param IND       -1 -> Fourier Transform, 1 -> Fourier Inverse Tranform
+	 *                  Oosaki Reference from OOSAKI spectrum analysis basic
+	 * 
+	 */
+	public Complex[] cfft(int N, Complex[] data, double samplingF, int ND, int IND) {
+		
+		// get power of 2 (NT) greater than N
+		int NT = 2;
+		while(NT < N) {
+			NT *= 2;
+		}
+		
+		Complex[] complex = new Complex[NT];
+		for (int i = 0; i < N; i++) {
+			complex[i] = new Complex(data[i].real, 0.);
+		}
+		if(NT != N) {
+			for(int i = N;i<NT;i++) {
+				complex[i] = new Complex(0.,0.);
+			}
+		}
+
+		
+		
+		int i = 1;
+		int j = 1;
+		int m = NT / 2;
+//		double deltaT = 1 / samplingF;
+
+		// Bit Traverse
+		for (i = 1; i < NT + 1; i++) {
+			if (i >= j) {
+				// goto 110
+			} else {
+				Complex temp = complex[j - 1];
+				complex[j - 1] = complex[i - 1];
+				complex[i - 1] = temp;
+			}
+
+			m = NT / 2; // 110
+			do {
+				if (j <= m) { // 120
+					// j = j + m;
+					break;
+					// goto 130
+				} else {
+					j = j - m;
+					m = m / 2;
+				}
+			} while (m >= 2);
+			j = j + m; // 130
+
+		}
+
+//		System.out.println();
+
+		int kmax = 1;
+		while (kmax < NT) {
+			int istep = kmax * 2;
+			for (int k = 1; k < kmax + 1; k++) {
+				Complex theta = new Complex(0., Math.PI * IND * (k - 1) / kmax);
+				for (int ii = k; ii <= NT; ii += istep) {
+					int jj = ii + kmax;
+					Complex tmp = complex[jj - 1].multiply(theta.cexp());
+					complex[jj - 1] = complex[ii - 1].diff(tmp);
+					complex[ii - 1] = complex[ii - 1].add(tmp);
+
+				}
+
+			}
+			kmax = istep;
+		}
+
+		for (int ii = 0; ii < complex.length; ii++) {
+			complex[ii] = complex[ii].divide(NT);  // N?
+		}
+
+		return complex;
+	}
+	
 	/**
 	 * FIR Complex Fourier Fast Transform
 	 * 
