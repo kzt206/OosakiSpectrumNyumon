a852e480f78d5395926432e7c74a203d80fbb9ee
kzt206 <kzt206@gmail.com>
2020-08-25 17:30:31 +0900

    Add fas method(Fourie amplitude spectrum)

--- OosakiSpectrumNyumon/src/fftFirst/FFT.java
+++ OosakiSpectrumNyumon/src/fftFirst/FFT.java
@@ -24,9 +24,8 @@ public class FFT {
 		this.coef_complex_fft = coef_complex_fft;
 	}
 
-	
 	/**
-	 * Complex Fourier Fast Transform modified for FAST  2020/8/25
+	 * Complex Fourier Fast Transform modified for FAST 2020/8/25
 	 * 
 	 * @param N         number of data
 	 * @param data      Input data
@@ -36,7 +35,7 @@ public class FFT {
 	 *                  Oosaki Reference from OOSAKI spectrum analysis basic
 	 * 
 	 */
-	public Complex[] fast(int N,Complex[] x,int nd,int IND) {
+	public Complex[] fast(int N, Complex[] x, int nd, int IND) {
 		Complex[] complex = new Complex[nd];
 		for (int i = 0; i < N; i++) {
 			complex[i] = new Complex(x[i].real, x[i].image);
@@ -97,8 +96,7 @@ public class FFT {
 
 		return complex;
 	}
-	
-	
+
 	/**
 	 * Complex Fourier Fast Transform
 	 * 
@@ -173,7 +171,7 @@ public class FFT {
 	}
 
 	/**
-	 * FIR  Complex Fourier Fast Transform
+	 * FIR Complex Fourier Fast Transform
 	 * 
 	 * @param N         number of data
 	 * @param data      Input data
@@ -247,9 +245,9 @@ public class FFT {
 
 		return x;
 	}
-	
+
 	/**
-	 * FIR 2 data Complex Fourier Fast Transform 
+	 * FIR 2 data Complex Fourier Fast Transform
 	 * 
 	 * @param N         number of data
 	 * @param data_r    Input data Real
@@ -260,7 +258,7 @@ public class FFT {
 	 *                  Oosaki Reference from OOSAKI spectrum analysis basic
 	 * 
 	 */
-	public double[][] fir_fft(int N, double[] data_r,double[] data_i, double samplingF, int ND, int IND) {
+	public double[][] fir_fft(int N, double[] data_r, double[] data_i, double samplingF, int ND, int IND) {
 		Complex[] complex = new Complex[data_r.length];
 		double[][] x = new double[data_r.length][2];
 		for (int i = 0; i < data_r.length; i++) {
@@ -324,9 +322,7 @@ public class FFT {
 
 		return x;
 	}
-	
-	
-	
+
 	/**
 	 * Finite Fourier Fast Transform
 	 * 
@@ -367,14 +363,58 @@ public class FFT {
 
 //			System.out.printf("%2d, f:%7.3f, A:%7.3f, B:%7.3f, AMP:%7.3e, PHASE:%7.3f ,FAS:%7.3f ,Power:%7.3f\n", ii,
 //					ii / (N * deltaT), coef[ii][0], coef[ii][1], amp, phase, fas, power);
-			System.out.printf("%10.8f, %10.8f\n",
-					ii / (N * deltaT), amp);
+			System.out.printf("%10.8f, %10.8f\n", ii / (N * deltaT), amp);
 
-			
-			
 		}
 
 		return coef;
+
+	}
+
+	/**
+	 * Fourier Amplitude Spectrum
+	 * 
+	 * @param N         number of data
+	 * @param data      Input data
+	 * @param samplingF Sampling frequency
+	 * @param ND        not used
+	 * @param IND       -1 -> Fourier Transform, 1 -> Fourier Inverse Tranform
+	 * 
+	 */
+	public double[][] fas(int N, double[] data, double samplingF, int ND) {
+		// IND : -1 -> Fourier Transform
+		// IND : 1 -> Fourier Inverse Tranform
+		// Reference from OOSAKI spectrum analysis basic
+
+		int IND = -1;
+
+		Complex[] complex = cfft(N, data, samplingF, ND, IND);
+		double deltaT = 1 / samplingF;
+
+		int number = N / 2 + 1;
+
+		double[][] coef = new double[number][2];
+		double[][] fas = new double[number][2];
+
+		System.out.println();
+		System.out.println("Fourie Transform");
+		// print out
+		for (int ii = 0; ii < complex.length / 2 + 1; ii++) {
+			fas[ii][0] = ii / (N * deltaT);
+			coef[ii][0] = 2 * complex[ii].real(); // Ak cos coeficient
+			coef[ii][1] = -2 * complex[ii].image(); // Bk sin coeficient
+			double amp = 2 * complex[ii].abs();
+			fas[ii][1] = amp * N * deltaT / 2;
+//			System.out.printf("%2d, f:%7.3f, A:%7.3f, B:%7.3f, AMP:%7.3f, PHASE:%7.3f ,FAS:%7.3f ,Power:%7.3f\n", ii,
+//					ii / (N * deltaT), coef[ii][0], coef[ii][1], amp, phase, fas, power);
+
+//			System.out.printf("%2d, f:%7.3f, A:%7.3f, B:%7.3f, AMP:%7.3e, PHASE:%7.3f ,FAS:%7.3f ,Power:%7.3f\n", ii,
+//					ii / (N * deltaT), coef[ii][0], coef[ii][1], amp, phase, fas, power);
+			System.out.printf("f:%10.8f, fas:%10.8f\n", fas[ii][0],fas[ii][1]);
+
+		}
+
+		return fas;
 
 	}
 
@@ -589,7 +629,7 @@ public class FFT {
 			b[offset + m] = 2. * fe * sinc(2. * Math.PI * fe * m);
 		}
 		for (int m = 0; m < j + 1 + 1; m++) {
-			b2[m] = b[m+1] * w[m];
+			b2[m] = b[m + 1] * w[m];
 		}
 
 		return b2;
@@ -610,7 +650,7 @@ public class FFT {
 			b[offset + m] = sinc(Math.PI * m) - 2. * fe * sinc(2. * Math.PI * fe * m);
 		}
 		for (int m = 1; m < j + 1 + 1; m++) {
-			b2[m] = b[m+1] * w[m];
+			b2[m] = b[m + 1] * w[m];
 		}
 
 		return b2;
@@ -629,10 +669,10 @@ public class FFT {
 		double[] b2 = new double[b.length];
 		int offset = (int) j / 2;
 		for (int m = -offset; m < offset + 1; m++) {
-			b[offset + m ] = 2. * fe2 * sinc(2. * Math.PI * fe2 * m) - 2. * fe1 * sinc(2. * Math.PI * fe1 * m);
+			b[offset + m] = 2. * fe2 * sinc(2. * Math.PI * fe2 * m) - 2. * fe1 * sinc(2. * Math.PI * fe1 * m);
 		}
-		for (int m = 0; m < j + 1 ; m++) {
-			b2[m] = b[m+1] * w[m];
+		for (int m = 0; m < j + 1; m++) {
+			b2[m] = b[m + 1] * w[m];
 		}
 
 		return b2;
@@ -663,18 +703,17 @@ public class FFT {
 	public double[] hannigWindow(double[] w, long n) {
 		double[] hanning = new double[w.length];
 		if (n % 2 == 0) {
-			for (int i = 0; i < n ; i++) {
+			for (int i = 0; i < n; i++) {
 				hanning[i] = 0.5 - 0.5 * Math.cos(2. * Math.PI * (i + 1) / n);
 			}
 		} else {
-			for (int i = 0; i < n ; i++) {
+			for (int i = 0; i < n; i++) {
 				hanning[i] = 0.5 - 0.5 * Math.cos(2. * Math.PI * (i + 0.5) / n);
 			}
 		}
 		return hanning;
 	}
 
-	
 	public static double[] filteringBPF(double[] waveData, int n, double dt, double lower, double upper, double alpha) {
 
 		double[] filteredData = new double[waveData.length];
@@ -774,5 +813,5 @@ public class FFT {
 		return filteredData;
 
 	}
-	
+
 }
